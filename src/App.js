cc728e6bdac6be3a5d443f9fbf154a4d17209f03
rahmed19 <ray.ahmed.sec@gmail.com>
2021-07-26 04:20:13 -0700

    getting data from firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import FirebaseContext from './context/firebase'
+import { firebase, FieldValue } from './lib/firebase'
 
 function App() {
+  // const dataFromFirebase = firebase.firestore().collection('sites')
 
+  const data = firebase.firestore().collection('sites').get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        let data = []
+
+      })
+    })
+
+
+  // snapshot.forEach(doc => {
+  //   console.log(doc.id, '=>', doc.data());
+  // });
   return (
     <div className="App">
-      <h1>This is timesheet</h1>
+      <h1>This is timesheet </h1>
     </div>
   );
 }

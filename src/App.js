057e354d7ebb19191a8791767de71ea3fb980654
rahmed19 <ray.ahmed.sec@gmail.com>
2021-07-26 05:42:33 -0700

    map over options

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import FirebaseContext from './context/firebase'
-import { firebase, FieldValue } from './lib/firebase'
+// import { firebase, FieldValue } from './lib/firebase'
+import { useContext, useState } from 'react'
 
 function App() {
+  const { firebase } = useContext(FirebaseContext)
   // const dataFromFirebase = firebase.firestore().collection('sites')
+  const [docs, setDocs] = useState([])
+  const [optionValue, setOptionValue] = useState([])
 
   const data = firebase.firestore().collection('sites').get()
     .then((snapshot) => {
+      let documents = []
       snapshot.forEach((doc) => {
-        let data = []
-
+        documents.push({ ...doc.data(), id: doc.id })
       })
+      setDocs(documents)
     })
 
 
-  // snapshot.forEach(doc => {
-  //   console.log(doc.id, '=>', doc.data());
-  // });
   return (
     <div className="App">
-      <h1>This is timesheet </h1>
+      <h1>This is a timesheet.</h1>
+      <form>
+        <b> Please select your site </b>
+        <select id="sitename" >
+          <option>--Select your site</option>
+          <option></option>
+          {docs && docs.map((doc) => {
+            return <option>{doc.sitename}</option>
+
+          })}
+        </select>
+      </form>
+
     </div>
   );
 }

1c55321d4fccf9c4e13bb494c1b0f902a27a13c1
rahmed19 <ray.ahmed.sec@gmail.com>
2021-07-29 02:28:53 -0700

    date fns

--- src/components/sitename.js
+++ src/components/sitename.js
@@ -43,7 +43,6 @@ export default function Sitename() {
                     return <option key={doc.id}>{doc.sitename}</option>
                 })}
             </select>
-            <br />
 
         </>
     )
--- src/components/timein-timeout.js
+++ src/components/timein-timeout.js
@@ -4,18 +4,35 @@ function TimeinTimeout() {
     const [timeIn, setTimeIn] = useState('')
     const [timeOut, setTimeOut] = useState('')
 
+    function handleTimeInChange(e) {
+        if ((/[a-zA-Z]/).test(e.target.value) === false) {
+            setTimeIn(e.target.value)
+        }
+
+    }
+
+    function handleTimeOutChange(e) {
+        if ((/[a-zA-Z]/).test(e.target.value) === false) {
+            setTimeOut(e.target.value)
+        } else
+            setTimeOut('')
+
+    }
+
     return (
         <>
             <input
                 aria-label="Enter your time in"
                 type="text"
-                onChange={({ target }) => setTimeIn(target.value)}
+                maxLength="4"
+                onChange={handleTimeInChange}
                 value={timeIn}
             />
             <input
                 aria-label="Enter your time out"
                 type="text"
-                onChange={({ target }) => setTimeOut(target.value)}
+                maxLength="4"
+                onChange={handleTimeOutChange}
                 value={timeOut}
             />
             {console.log(((Number(timeOut) - Number(timeIn)) / 100).toFixed(2))}

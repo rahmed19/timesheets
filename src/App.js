fa18871e7cc201817415e9242e3781d0a4b818ee
rahmed19 <ray.ahmed.sec@gmail.com>
2021-08-11 04:24:44 -0700

    signup form setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import React, { useState } from 'react'
 import './App.css';
 import Timesheets from './pages/timesheets';
+import Signup from './pages/signup';
 
 function App() {
   return (
     <>
-      <Timesheets />
+      {/* <Timesheets /> */}
+      <Signup />
     </>
   );
 }
---
+++ src/components/signup-form.js
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react'
+
+export default function SignupForm() {
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+
+
+    return (
+        <>
+            Sign up.
+            <form>
+                Email address:<input type="email" ref={emailRef} required /><br />
+                Password: <input type="password" ref={passwordRef} required /><br />
+                Confirm Password: <input type="password" ref={passwordConfirmRef} required /><br />
+                <button type="submit" >Sign up</button>
+            </form>
+
+        </>
+    )
+}

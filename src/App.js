766171a367ff22efc9dbe2883970986d125d17c9
rahmed19 <ray.ahmed.sec@gmail.com>
2021-08-12 06:31:41 -0700

    refactoring auth listener

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import './App.css';
 import Timesheets from './pages/timesheets';
 import Signup from './pages/signup';
-import useAuthListener from './hooks/use-auth-listener'
+import useAuthListener from './context/use-auth-listener'
 
 function App() {
   const { user } = useAuthListener()
--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -1,20 +1,52 @@
-import React, { useRef } from 'react'
+import { set } from 'date-fns'
+import React, { useRef, useState, useContext, useEffect } from 'react'
+import UseAuthListener from '../context/use-auth-listener'
+import FirebaseContext from '../context/firebase'
 
 export default function SignupForm() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+    const { auth } = useContext(FirebaseContext)
+
+
+    const { signup } = UseAuthListener
+
+    // function signup(email, password) {
+    //     return auth.createUserWithEmailAndPassword(email, password)
+    // }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('Passwords do not match')
+        }
+        try {
+            setError('')
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+        } catch (error) {
+            setError(error.message)
+        }
+        setLoading(false)
+    }
 
 
     return (
         <>
-            Sign up.
-            <form>
+            Sign up.<br />
+            {error}<br />
+            {/* Current User: {user} */}
+
+            <form onSubmit={handleSubmit}>
                 Email address:<input type="email" ref={emailRef} required /><br />
                 Password: <input type="password" ref={passwordRef} required /><br />
                 Confirm Password: <input type="password" ref={passwordConfirmRef} required /><br />
-                <button type="submit" >Sign up</button> <br />
+                <button type="submit" disabled={loading}>Sign up</button> <br />
                 Already have an account? Log in.
             </form>
 

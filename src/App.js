f4ac79eb96d98cd81aeea91d83199566da9fad62
rahmed19 <ray.ahmed.sec@gmail.com>
2021-08-16 04:17:29 -0700

    login setup

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import React, { useState } from 'react'
 import './App.css';
 import Timesheets from './pages/timesheets';
 import Signup from './pages/signup';
+import Login from './pages/login';
 import { AuthProvider } from './context/auth-context'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        {/* <Timesheets /> */}
-        <Signup />
-      </AuthProvider>
+      <Router>
+        <AuthProvider>
+          <Switch >
+            <Route exact path="/" component={Timesheets} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+
+          </Switch>
+
+
+        </AuthProvider>
+      </Router>
     </>
   );
 }
---
+++ src/components/login-form.js
@@ -0,0 +1,49 @@
+import { set } from 'date-fns'
+import React, { useRef, useState, useContext, useEffect } from 'react'
+import FirebaseContext from '../context/firebase'
+import { useAuth } from '../context/auth-context'
+
+export default function LoginForm() {
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+    const { signup, currentUser } = useAuth()
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('Passwords do not match')
+        }
+        try {
+            setError('')
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+        } catch (error) {
+            setError(error.message)
+        }
+        setLoading(false)
+    }
+
+
+    return (
+        <>
+            Sign up.<br />
+            {error}<br />
+            {/* Current User: {user} */}
+
+            <form onSubmit={handleSubmit}>
+                Email address:<input type="email" ref={emailRef} required /><br />
+                Password: <input type="password" ref={passwordRef} required /><br />
+                Confirm Password: <input type="password" ref={passwordConfirmRef} required /><br />
+                <button type="submit" disabled={loading}>Sign up</button> <br />
+                Already have an account? Log in.
+            </form>
+
+
+        </>
+    )
+}

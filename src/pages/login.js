99e092ee092ada760c0d50e454860681c9ebed84
rahmed19 <ray.ahmed.sec@gmail.com>
2021-08-25 05:37:13 -0700

    tailwind config

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,7 +4,9 @@ import LoginForm from '../components/login-form'
 export default function Login() {
     return (
         <>
+        <div className="container mx-auto">
             <LoginForm />
+        </div>
         </>
     )
 }

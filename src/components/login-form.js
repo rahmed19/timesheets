73ea73589381c4bc80be458ec2c4b9e9894c9258
rahmed19 <ray.ahmed.sec@gmail.com>
2021-08-26 06:12:05 -0700

    naavbar menu update

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -29,27 +29,25 @@ export default function LoginForm() {
 	if (!currentUser) {
 		return (
 			<>
-				<div className='flex h-screen justify-center items-top bg-gray-100'>
-					<div>
-						Log In.
-						<br />
-					</div>
-					{error}
+				<div>
+					Log In.
 					<br />
-
-					<form onSubmit={handleSubmit}>
-						Test Input: <TailwindInput type='email' ref={emailRef} />
-						<br />
-						Password: <TailwindInput type='password' ref={passwordRef} /> <br />
-						<button type='submit' disabled={loading}>
-							Log In
-						</button>{" "}
-						<br />
-						<Link to='/forgot-password'>Forgot Password?</Link>
-						<br />
-						Don't have an account? <Link to='/signup'>Sign up</Link>
-					</form>
 				</div>
+				{error}
+				<br />
+
+				<form onSubmit={handleSubmit}>
+					Test Input: <TailwindInput type='email' ref={emailRef} />
+					<br />
+					Password: <TailwindInput type='password' ref={passwordRef} /> <br />
+					<button type='submit' disabled={loading}>
+						Log In
+					</button>{" "}
+					<br />
+					<Link to='/forgot-password'>Forgot Password?</Link>
+					<br />
+					Don't have an account? <Link to='/signup'>Sign up</Link>
+				</form>
 			</>
 		)
 		// if not logged in then redirect to login page
--- src/components/sections/navbar.js
+++ src/components/sections/navbar.js
@@ -1,5 +1,62 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import { useAuth } from "../../context/auth-context"
 
 export default function Navbar() {
-	return <div>NavBar</div>
+	const { currentUser, logout } = useAuth()
+	return (
+		<>
+			<nav
+				className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono'
+				role='navigation'
+			>
+				<Link to='/' className='pl-8'>
+					True Canadian Security Employee Portal
+				</Link>
+				<div className='px-4 cursor-pointer md:hidden'>
+					<svg
+						className='w-6 h-6'
+						fill='none'
+						stroke='currentColor'
+						viewBox='0 0 24 24'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							strokeWidth={2}
+							d='M4 6h16M4 12h16M4 18h16'
+						/>
+					</svg>
+				</div>
+				<div className='pr-8 md:block hidden'>
+					<Link className='p-4' to='/'>
+						Home
+					</Link>
+					{!currentUser && (
+						<Link className='p-4' to='/login'>
+							Login
+						</Link>
+					)}
+					{!currentUser && (
+						<Link className='p-4' to='/signup'>
+							Sign Up
+						</Link>
+					)}
+
+					{currentUser && (
+						<Link className='p-4' to='/update-profile'>
+							Update Profile
+						</Link>
+					)}
+
+					{currentUser && (
+						<Link to='/login' onClick={() => logout()}>
+							Log Out
+						</Link>
+					)}
+				</div>
+			</nav>
+		</>
+	)
 }

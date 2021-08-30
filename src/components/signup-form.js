b878429aab5cc52a940b79f82b4705c3ba774c98
rahmed19 <ray.ahmed.sec@gmail.com>
2021-08-30 01:54:38 -0700

    signup form CSS

--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -1,68 +1,85 @@
-import React, { useRef, useState } from 'react'
-import { useAuth } from '../context/auth-context'
-import { Link, useHistory, Redirect } from 'react-router-dom'
-import TailwindInput from '../hooks/tailwind/tailwindInput'
+import React, { useRef, useState } from "react"
+import { useAuth } from "../context/auth-context"
+import { Link, useHistory, Redirect } from "react-router-dom"
+import TailwindInput from "../hooks/tailwind/tailwindInput"
 
 export default function SignupForm() {
+	const emailRef = useRef()
+	const passwordRef = useRef()
+	const passwordConfirmRef = useRef()
+	const firstNameRef = useRef()
+	const lastNameRef = useRef()
+	const [error, setError] = useState("")
+	const [loading, setLoading] = useState(false)
+	const { signup, currentUser } = useAuth()
+	const history = useHistory()
 
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const firstNameRef = useRef()
-    const lastNameRef = useRef()
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState(false)
-    const { signup, currentUser } = useAuth()
-    const history = useHistory()
+	async function handleSubmit(e) {
+		e.preventDefault()
 
-    async function handleSubmit(e) {
-        e.preventDefault()
+		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+			return setError("Passwords do not match")
+		}
+		try {
+			setError("")
+			setLoading(true)
+			await signup(
+				emailRef.current.value,
+				passwordRef.current.value,
+				firstNameRef.current.value,
+				lastNameRef.current.value
+			)
+			history.push("/")
+		} catch (error) {
+			setError(error.message)
+		}
+		setLoading(false)
+	}
+	//check to see if logged in
+	if (!currentUser) {
+		return (
+			<>
+				<div className='flex flex-wrap items-center justify-between'>
+					<div></div>
 
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError('Passwords do not match')
-        }
-        try {
-            setError('')
-            setLoading(true)
-            await signup(emailRef.current.value,
-                passwordRef.current.value,
-                firstNameRef.current.value,
-                lastNameRef.current.value)
-            history.push("/")
-        } catch (error) {
-            setError(error.message)
-        }
-        setLoading(false)
-    }
-    //check to see if logged in
-    if (!currentUser) {
-        return (
-            <>
-                Sign up.<br />
-                {error}<br />
-
-                <form onSubmit={handleSubmit}>
-                    {/* Email address:<input type="email" ref={emailRef} required /><br /> */}
-                    Email Address: <TailwindInput required="required" type="email" ref={emailRef} /> <br />
-                    Password: <input type="password" ref={passwordRef} required /><br />
-                    Confirm Password: <input type="password" ref={passwordConfirmRef} required /><br />
-                    First Name: <input ref={firstNameRef} required /><br />
-                    Last Name: <input ref={lastNameRef} required /><br />
-                    <button type="submit" disabled={loading}>Sign up</button> <br />
-                    Already have an account? <Link to="/login">Log In</Link>
-                </form>
-
-
-            </>
-        )
-    }
-    // if not logged in then redirect to login page
-    else if (currentUser) {
-        return (
-            <>
-                <Redirect to="/" />
-            </>
-        )
-    }
-
+					<div className='w-full max-w-xs conter-center'>
+						<form
+							onSubmit={handleSubmit}
+							className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+						>
+							<br />
+							{error}
+							<br />
+							Email address:
+							<input type='email' ref={emailRef} required className='' />
+							<br />
+							Password: <input type='password' ref={passwordRef} required />
+							<br />
+							Confirm Password:{" "}
+							<input type='password' ref={passwordConfirmRef} required />
+							<br />
+							First Name: <input ref={firstNameRef} required />
+							<br />
+							Last Name: <input ref={lastNameRef} required />
+							<br />
+							<button type='submit' disabled={loading}>
+								Sign up
+							</button>{" "}
+							<br />
+							Already have an account? <Link to='/login'>Log In</Link>
+						</form>
+					</div>
+					<div></div>
+				</div>
+			</>
+		)
+	}
+	// if not logged in then redirect to login page
+	else if (currentUser) {
+		return (
+			<>
+				<Redirect to='/' />
+			</>
+		)
+	}
 }

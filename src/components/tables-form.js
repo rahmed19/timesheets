6edbdf3fc23e838ee902326087494c636c472d12
rahmed19 <ray.ahmed.sec@gmail.com>
2021-09-13 03:14:30 -0700

    dynamic id

--- src/components/tables-form.js
+++ src/components/tables-form.js
@@ -12,9 +12,9 @@ const TablesForm = () => {
 		},
 	])
 
-	const handleAddRow = index => {
+	const handleAddRow = i => {
 		const list = [...allData]
-		list.splice(index + 1, 0, {
+		list.splice(i + 1, 0, {
 			date: "",
 			siteName: "",
 			signIn: "",
@@ -24,16 +24,16 @@ const TablesForm = () => {
 		setAllData(list)
 	}
 
-	const handleRemoveRow = index => {
+	const handleRemoveRow = i => {
 		const list = [...allData]
-		list.splice(index, 1)
+		list.splice(i, 1)
 		setAllData(list)
 	}
 
-	const handleInputChange = (e, index) => {
+	const handleInputChange = (e, i) => {
 		const { name, value } = e.target
 		const list = [...allData]
-		list[index][name] = value
+		list[i][name] = value
 		setAllData(list)
 	}
 
@@ -85,17 +85,44 @@ const TablesForm = () => {
 		}
 	}, [])
 
+	// for (let i = 0; i < dateCounter; i++) {
+	// 	//formatted dates
+	// 	let dateContents = document.getElementById(`date-${i}`)
+	// 	dateContents && allDatesArray.push(dateContents.innerText)
+
+	// 	//sitename
+	// 	let sitenameContents = document.getElementById(`sitename-${i}`)
+	// 	sitenameContents && allSitenamesArray.push(sitenameContents.value)
+
+	// 	//signIn
+	// 	let signInContents = document.getElementById(`signIn-${i}`)
+	// 	signInContents && allSignInArray.push(signInContents.value)
+
+	// 	//signOut
+	// 	let signOutContents = document.getElementById(`signOut-${i}`)
+	// 	signOutContents && allSignOutArray.push(signOutContents.value)
+
+	// 	//hoursWorked
+	// 	let hoursWorkedContents = document.getElementById()
+	// 	hoursWorkedContents && allHoursWorkedArray.push(hoursWorkedContents.value)
+	// }
+	// //totalWeeklyHours
+
+	// let totalWeeklyHoursContents = document.getElementById("totalWeeklyHours")
+	// allTotalWeeklyHours = parseInt(totalWeeklyHoursContents.value)
+
 	return (
 		<>
 			<div>
-				{allData.map((data, index) => {
+				{allData.map((data, i) => {
 					return (
 						<div>
 							<select
+								//id={}
 								type='text'
 								name='date'
 								className='border bg-gray-300 ml-3'
-								onChange={e => handleInputChange(e, index)}
+								onChange={e => handleInputChange(e, i)}
 								value={data.date}
 							>
 								<option>---Select the date</option>
@@ -105,41 +132,45 @@ const TablesForm = () => {
 							</select>
 
 							<input
+								id={`sitename-${i}`}
 								type='text'
 								name='siteName'
 								placeholder=''
 								value={data.siteName}
 								className='border bg-gray-300 ml-3'
-								onChange={e => handleInputChange(e, index)}
+								onChange={e => handleInputChange(e, i)}
 							/>
 							<input
+								id={`signIn-${i}`}
 								type='text'
 								name='signIn'
 								placeholder=''
 								value={data.signIn}
 								className='border bg-gray-300 ml-3'
-								onChange={e => handleInputChange(e, index)}
+								onChange={e => handleInputChange(e, i)}
 							/>
 							<input
+								id={`signOut-${i}`}
 								type='text'
 								name='signOut'
 								placeholder=''
 								value={data.signOut}
 								className='border bg-gray-300 ml-3'
-								onChange={e => handleInputChange(e, index)}
+								onChange={e => handleInputChange(e, i)}
 							/>
 							<input
+								id={`hoursWorked-${i}`}
 								type='text'
 								name='hoursWorked'
 								placeholder=''
 								value={data.hoursWorked}
 								className='border bg-gray-300 ml-3'
-								onChange={e => handleInputChange(e, index)}
+								onChange={e => handleInputChange(e, i)}
 							/>
-							<button onClick={() => handleAddRow(index)} className='ml-3'>
+							<button onClick={() => handleAddRow(i)} className='ml-3'>
 								Add Row
 							</button>
-							<button onClick={() => handleRemoveRow(index)} className='ml-3'>
+							<button onClick={() => handleRemoveRow(i)} className='ml-3'>
 								Remove Row
 							</button>
 						</div>
